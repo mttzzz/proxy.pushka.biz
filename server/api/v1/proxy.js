5de1845f77a808906ab61b1c965f5105011c51a9
Kirill Egorov <kirill_egorov@me.com>
2022-12-22 10:47:34 +0300

    first commit

---
+++ server/api/v1/proxy.js
@@ -0,0 +1,34 @@
+export default defineEventHandler(async (event) =>{
+   
+  const {original_req_url: url, authorization} = event.node.req.headers
+  
+  if (!url) {
+    return sendError(
+      event,
+      createError({ statusCode: 400, statusMessage: "No 'original_req_url' provided in headers" })
+      );
+    }
+    
+    
+    const params = getQuery(event)
+    const method = event.node.req.method
+    const options = {method, params, headers: {}}
+
+    if (method !== 'GET') {
+      options.body = await readBody(event)
+    }
+    if (authorization) {
+      options.headers.authorization = authorization
+    }
+    
+  try {
+    return await $fetch(url, options)
+ 
+  } catch (error) {
+
+    return sendError(
+        event,
+        createError({ statusCode: 500, statusMessage: error.message })
+      );
+  }
+})
