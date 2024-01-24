9a5527bd9ddbdccb1f6383226a7b15302ba40abc
Kirill Egorov <kirill_egorov@me.com>
2024-01-24 17:07:14 +0300

    update server/api/chatgpt/index.post.js

--- server/api/chatgpt/index.post.js
+++ server/api/chatgpt/index.post.js
@@ -15,13 +15,7 @@ export default defineEventHandler(async (event) => {
         return 'No messages defined'
       }
 
-      const completion = await openai.chat.completions.create({
-        messages: [{"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": "Where was it played?"}],
-        model: "gpt-3.5-turbo",
-      });
+      const completion = await openai.chat.completions.create({messages,model});
       return completion.choices[0].message.content
       } catch (err) {
        
