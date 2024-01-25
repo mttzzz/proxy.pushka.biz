47d7e05cd3125ef05e483e1d71a5f34defbd6268
Kirill Egorov <kirill_egorov@me.com>
2024-01-25 18:33:23 +0300

    update server/api/chatgpt/index.post.js

--- server/api/chatgpt/index.post.js
+++ server/api/chatgpt/index.post.js
@@ -5,29 +5,84 @@ export default defineEventHandler(async (event) => {
   const openai = new OpenAI();
    
     try {
-      const {model, message, system} = await readBody(event)
+      const {sensei_hash,model,system, message  } = await readBody(event) 
+      
+
+      
 
       if (!model)  {
-        return 'No model defined'
-      }
+        return sendError(
+          event,
+          createError({ statusCode: 500, statusMessage: 'No model defined'})
+          );
+        }
+    
 
       if (!system)  {
-        return 'No system defined'
-      }
+        return sendError(
+          event,
+          createError({ statusCode: 500, statusMessage: 'No system defined'})
+          );
+        }
+      
+      
 
       if (!message)  {
-        return 'No system defined'
-      }
-
-      const completion = await openai.chat.completions.create({model, messages : [
+        return sendError(
+          event,
+          createError({ statusCode: 500, statusMessage: 'No system defined'})
+          );
+        }
+     
+    
+      console.log('');
+      
+      openai.chat.completions.create({model, messages : [
         {
           "role": "user",
           "content": message
         },
-      ]});
-      return completion.choices[0].message.content
-      } catch (err) {
+      ]}).then(completion => {
+        const content = completion.choices[0].message.content
+        let parsedContent = null
+        try {
+          parsedContent = JSON.parse(content)
+        } catch (error) {
+          console.log(error);
+        }
+        
+         if (!parsedContent) {
+          throw new Error('Не валидный JSON');
+         }
+        $fetch('https://api.sensei.plus/webhook', { method:'post', params: {
+          hash: sensei_hash,
+          result: 'success',
+        }, body: {params: {local: {
+          JSON: content,
+          'Цели':parsedContent?.data?.targets,
+          'Задачи':parsedContent?.data?.tasks,
+          'Обязательства':parsedContent?.data?.need,
+        }}}
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
+        
        
+      });
+
+      return 'ok'
+      } catch (err) {
+        $fetch('https://api.sensei.plus/webhook', { method:'post', params: {
+          hash: sensei_hash,
+          result: 'error',
+        }, body: {
+         status: 500,
+         error: err.message
+        }
+      })
+
         return sendError(
           event,
           createError({ statusCode: 500, statusMessage: err.message})
