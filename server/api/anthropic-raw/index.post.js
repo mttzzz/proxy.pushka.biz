99212e542d8dd9ba3fc939775e73c8e98899c936
Kirill Egorov <kirill_egorov@me.com>
2024-09-03 12:51:23 +0300

    feat: create server/api/anthropic-raw/index.post.js

---
+++ server/api/anthropic-raw/index.post.js
@@ -0,0 +1,50 @@
+export default defineEventHandler(async (event) => {
+  const anthropic = event.context.anthropic
+  let operationOutcome = { success: true, message: 'ok' }
+  const { sensei_hash, model, system, message } = await readBody(event)
+
+  try {
+    const completion = await anthropic.messages.create({
+      model,
+      max_tokens: 4096,
+      system,
+      messages: [
+        { role: 'user', content: message },
+      ],
+    })
+
+    const content = completion.content[0].text
+
+    // Prepare response content for the success case to send to Sensei
+    operationOutcome.responseContent = {
+      params: { local: { answer: content } },
+    }
+  }
+  catch (err) {
+    operationOutcome = {
+      success: false,
+      message: err.message,
+      status: err.status,
+    }
+  }
+  finally {
+    if (sensei_hash) {
+      await $fetch('https://api.sensei.plus/webhook', {
+        method: 'post',
+        params: {
+          hash: sensei_hash,
+          result: operationOutcome.success ? 'success' : 'error',
+        },
+        body: operationOutcome.responseContent,
+      }).catch((finalErr) => {
+        operationOutcome = {
+          success: false,
+          message: finalErr.message,
+          status: finalErr.status,
+        }
+      })
+    }
+  }
+
+  return operationOutcome
+})
