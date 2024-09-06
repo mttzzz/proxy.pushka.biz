da28d1f126e7c760876a9903be2b373d34a2184a
Kirill Egorov <kirill_egorov@me.com>
2024-09-06 20:29:27 +0300

    Update dependencies in package.json

--- server/api/anthropic-raw/index.post.js
+++ server/api/anthropic-raw/index.post.js
@@ -1,20 +1,28 @@
 export default defineEventHandler(async (event) => {
   const anthropic = event.context.anthropic
   let operationOutcome = { success: true, message: 'ok' }
-  const { sensei_hash, model, system, message } = await readBody(event)
+  const { sensei_hash, model, system, message, temperature, top_p } = await readBody(event)
 
   try {
-    const completion = await anthropic.messages.create({
+    const completion = await anthropic.beta.promptCaching.messages.create({
       model,
       max_tokens: 8192,
-      system,
+      top_p: Number.parseFloat(top_p) || 1.0,
+      temperature: Number.parseFloat(temperature) || 1.0,
+      system: [
+        {
+          type: 'text',
+          text: system,
+          cache_control: { type: 'ephemeral' },
+        },
+      ],
       messages: [
         { role: 'user', content: message },
       ],
     })
 
     const content = completion.content[0].text
-
+    operationOutcome.completion = completion
     // Prepare response content for the success case to send to Sensei
     operationOutcome.responseContent = {
       params: { local: { answer: content } },
